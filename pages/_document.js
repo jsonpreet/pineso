beb296637526162375efe4563d3c0210dab67168
MrPreet <harpreet9629@gmail.com>
2022-10-12 14:46:22 +0530

    social tags

--- pages/_document.js
+++ pages/_document.js
@@ -22,18 +22,33 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-            <Head>
-                <meta name="description" content="Build with Deso Blockchain" />
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" /> 
-                {CssBaseline.flush()}
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
+                <Head>
+                    <meta name="robots" content="index, follow" />
+                    <meta name="description" content="Build with Deso Blockchain" />
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:site" content="@pinesoio" />
+                    <meta name="twitter:creator" content="@pinesoio" />
+                    <meta property="og:url" content="https://pineso.io" />
+                    <meta property="og:title" content="Pineso" />
+                    <meta property="og:description" content="Build with Deso Blockchain" />
+                    <meta property="og:image" content="https://pineso.io/images/og-image.png" />
+                    <meta property="og:image:alt" content="Pineso" />
+                    <meta property="og:image:width" content="1200" />
+                    <meta property="og:image:height" content="630" />
+                    <meta property="og:site_name" content="Pineso" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:locale" content="en_US" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" /> 
+                    {CssBaseline.flush()}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
             </Html>
         )
     }

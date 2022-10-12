5f15fa20fc294e08ccec150e9e5583e56b4d4a6d
MrPreet <harpreet9629@gmail.com>
2022-10-12 15:03:03 +0530

    no its not

--- pages/about.jsx
+++ pages/about.jsx
@@ -11,7 +11,7 @@ const AboutPage = () => {
                 <meta property="og:url" content={BASE_URL} />
                 <meta property="og:title" content="Pineso" />
                 <meta property="og:description" content="Build with Deso Blockchain" />
-                <meta property="og:image" content={`${BASE_URL}/images/icon-512x512.png`} />
+                <meta property="og:image" content={`${BASE_URL}/images/icons/icon-512x512.png`} />
             </Head>
             <Layout>
                 <h1>Page</h1>
--- pages/index.jsx
+++ pages/index.jsx
@@ -24,7 +24,7 @@ const Home = () => {
           <meta property="og:url" content={BASE_URL} />
           <meta property="og:title" content="Pineso" />
           <meta property="og:description" content="Build with Deso Blockchain" />
-          <meta property="og:image" content={`${BASE_URL}/images/icon-512x512.png`} />
+          <meta property="og:image" content={`${BASE_URL}/images/icons/icon-512x512.png`} />
       </Head>
       <Layout>
           <Suspense fallback={<LoadingLoader message={`${isLoggedIn ? `Loading Following Pins for you.` : `Loading Hot Pins for you.`}`} />}>

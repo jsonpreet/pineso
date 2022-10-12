7a105fc69e8ee243587110e3f569efae72d13066
MrPreet <harpreet9629@gmail.com>
2022-10-12 14:23:07 +0530

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URI = process.env.NODE_API_URL || 'https://node.deso.org/api/v0';
-export const BASE_IDENTITY_URI = process.env.NODE_IDENTITY_URL || 'https://identity.deso.org';
-export const EXTERNAL_LINK = process.env.EXTERNAL_URL || 'https://diamondapp.com';
-export const BASE_URL = process.env.BASE_URL || 'https://pineso.io';
+export const BASE_URI = process.env.NEXT_PUBLIC_NODE_API_URL || 'https://node.deso.org/api/v0';
+export const BASE_IDENTITY_URI = process.env.NEXT_PUBLIC_NODE_IDENTITY_URL || 'https://identity.deso.org';
+export const EXTERNAL_LINK = process.env.NEXT_PUBLIC_EXTERNAL_URL || 'https://diamondapp.com';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://pineso.io';
 
 export const config = {
     defaultOptions: {

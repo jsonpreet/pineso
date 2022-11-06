761b0cab88f18f8a725fc9bf4ba15a1e2e2f03ac
MrPreet <harpreet9629@gmail.com>
2022-11-06 20:03:43 +0530

    Fixed Empty Posts from Trending Tag

--- src/data/trending-tags.js
+++ src/data/trending-tags.js
@@ -12,82 +12,84 @@ const getSingleTagFeed = async (tag) => {
         SortByNew: true,
         Tag: tag.Hashtag,
     })
+    console.log(response.data)
     if (response === null) {
         return null
     } else {
         const posts = response.data.HotFeedPage;
+        if (posts !== null && posts.length > 0) {
+            const filtered = posts.filter(post => {
+                return post.ImageURLs !== null && post.ImageURLs[0] !== '' && post.ImageURLs[0] !== undefined;
+            });
 
-        const filtered = posts.filter(post => {
-            return post.ImageURLs !== null && post.ImageURLs[0] !== '' && post.ImageURLs[0] !== undefined;
-        });
-
-        filtered.map(async (post) => {
-            post.tag = tag;
-        });
-        const random = filtered.sort(function () { return 0.5 - Math.random() });
-        // const numbers = [0, 1, 2, 3, 4];
-        // return numbers.map((number) => {
-        //     if (filtered[number].ImageURLs !== null && filtered[number].ImageURLs[0] !== '' && filtered[number].ImageURLs[0] !== undefined) {
-        //         const ext = get_url_extension(filtered[number].ImageURLs[0]);
-        //         if (SUPPORTED_FORMATS.includes(ext)) {
-        //             return filtered[number]
-        //         }
-        //     }
-        // })
-        if (random[0].ImageURLs !== null && random[0].ImageURLs[0] !== '' && random[0].ImageURLs[0] !== undefined) {
-            const ext = get_url_extension(random[0].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[0]
+            filtered.map(async (post) => {
+                post.tag = tag;
+            });
+            const random = filtered.sort(function () { return 0.5 - Math.random() });
+            // const numbers = [0, 1, 2, 3, 4];
+            // return numbers.map((number) => {
+            //     if (filtered[number].ImageURLs !== null && filtered[number].ImageURLs[0] !== '' && filtered[number].ImageURLs[0] !== undefined) {
+            //         const ext = get_url_extension(filtered[number].ImageURLs[0]);
+            //         if (SUPPORTED_FORMATS.includes(ext)) {
+            //             return filtered[number]
+            //         }
+            //     }
+            // })
+            if (random[0].ImageURLs !== null && random[0].ImageURLs[0] !== '' && random[0].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[0].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[0]
+                }
             }
-        }
-        else if (random[1].ImageURLs !== null && random[1].ImageURLs[0] !== '' && random[1].ImageURLs[0] !== undefined) {
-            const ext = get_url_extension(random[1].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[1]
+            else if (random[1].ImageURLs !== null && random[1].ImageURLs[0] !== '' && random[1].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[1].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[1]
+                }
+            } else if (random[2].ImageURLs !== null && random[2].ImageURLs[0] !== '' && random[2].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[2].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[2]
+                }
+            } else if (random[3].ImageURLs !== null && random[3].ImageURLs[0] !== '' && random[3].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[3].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[3]
+                }
+            } else if (random[4].ImageURLs !== null && random[4].ImageURLs[0] !== '' && random[4].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[4].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[4]
+                }
+            } else if (random[5].ImageURLs !== null && random[5].ImageURLs[0] !== '' && random[5].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[5].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[5]
+                }
             }
-        } else if (random[2].ImageURLs !== null && random[2].ImageURLs[0] !== '' && random[2].ImageURLs[0] !== undefined) {
-           const ext = get_url_extension(random[2].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[2]
+            else if (random[6].ImageURLs !== null && random[6].ImageURLs[0] !== '' && random[6].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[6].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[6]
+                }
+            } else if (random[7].ImageURLs !== null && random[7].ImageURLs[0] !== '' && random[7].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[7].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[7]
+                }
+            } else if (random[8].ImageURLs !== null && random[8].ImageURLs[0] !== '' && random[8].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[3].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[8]
+                }
+            } else if (random[9].ImageURLs !== null && random[9].ImageURLs[0] !== '' && random[9].ImageURLs[0] !== undefined) {
+                const ext = get_url_extension(random[9].ImageURLs[0]);
+                if (SUPPORTED_FORMATS.includes(ext)) {
+                    return random[9]
+                }
+            } else {
+                return null
             }
-        } else if (random[3].ImageURLs !== null && random[3].ImageURLs[0] !== '' && random[3].ImageURLs[0] !== undefined) {
-           const ext = get_url_extension(random[3].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[3]
-            }
-        } else if (random[4].ImageURLs !== null && random[4].ImageURLs[0] !== '' && random[4].ImageURLs[0] !== undefined) {
-           const ext = get_url_extension(random[4].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[4]
-            }
-        } else if (random[5].ImageURLs !== null && random[5].ImageURLs[0] !== '' && random[5].ImageURLs[0] !== undefined) {
-            const ext = get_url_extension(random[5].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[5]
-            }
-        }
-        else if (random[6].ImageURLs !== null && random[6].ImageURLs[0] !== '' && random[6].ImageURLs[0] !== undefined) {
-            const ext = get_url_extension(random[6].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[6]
-            }
-        } else if (random[7].ImageURLs !== null && random[7].ImageURLs[0] !== '' && random[7].ImageURLs[0] !== undefined) {
-           const ext = get_url_extension(random[7].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[7]
-            }
-        } else if (random[8].ImageURLs !== null && random[8].ImageURLs[0] !== '' && random[8].ImageURLs[0] !== undefined) {
-           const ext = get_url_extension(random[3].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[8]
-            }
-        } else if (random[9].ImageURLs !== null && random[9].ImageURLs[0] !== '' && random[9].ImageURLs[0] !== undefined) {
-           const ext = get_url_extension(random[9].ImageURLs[0]);
-            if (SUPPORTED_FORMATS.includes(ext)) {
-                return random[9]
-            }
-        } else {
-            return null
         }
     }
 }
